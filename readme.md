3d6b1b5b406f31b91728b6feff1ac5b3af4846f8
frkatona <apk5101@psu.edu>
2024-05-29 11:56:44 -0400

    updated statistics for paper revision

--- readme.md
+++ readme.md
@@ -2,15 +2,33 @@
 
 This project uses Flory-Huggins theory to estimate the number of polymer chains in PDMS after a solvent wash. 
 
- ## Flory-Huggins theory
+## Output
 
- Flory-Huggins theory models the free energy of mixing for a polymer solution (here, PDMS in hexane).  This can be used to estimate the number of polymer chains in a given volume of solvent, n:
+### Example Crosslink Density Output
+![example output](images/example_output.png "Example Output")
+
+### Example Tukey HSD Test Output (console)
+
+![example output](images/example_tukey.png "Tukey HSD Test")
+
+## Usage
+
+for use with custom csv format where:
+  - row 1 is "sample, pre-wash, wash, post-dry"
+  - sample names are formatted as "this_1", "this_2", "that_1", "that_2", etc.
+  - mass is in grams
+
+![example csv](images/example_csv.png "Example CSV Format")
+
+## Flory-Huggins theory
+
+Flory-Huggins theory models the free energy of mixing for a polymer solution (here, PDMS in hexane).  This can be used to estimate the number of polymer chains in a given volume of solvent, n:
 
 $$
-n = \frac{ln(1-v) + v+ \chi v^{2}}{V(v - \frac{v}{2})}
+n = \frac{ln(1-v) + v+ \chi v^{2}}{V(v^{1/3} - \frac{v}{2})}
 $$
 
-where $v$ ($v_{poly}$) is the volume fraction of polymer in the swollen mass, $\chi$ ($\chi_1$) is the system-dependent Flory-Huggins interaction parameter, and $V$ ($V_{mol,solv}$) is the molar volume of the solvent.
+where $v$ ($v_{poly}$) is the volume fraction of polymer in the swollen mass, $\chi$ ($\chi_1$) is the system-dependent Flory-Huggins interaction parameter, and $V$ ($V_{mol,solv}$) is the molar volume of the solvent ([Fanse et al., 2022](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9208241/)).
 
 The value $n$ is useful directly, but it can also be related to the elastic modulus ($E$) of the polymer by the equation:
 
@@ -20,16 +38,5 @@ $$
 
 where $R$ is the gas constant and $T$ is the absolute temperature.
 
-## Usage
-for use with custom csv format where:
-   - row 1 is "sample, pre-wash, wash, post-dry"
-   - sample names are formatted as "this_1", "this_2", "that_1", "that_2", etc.
-   - mass is in grams
-
- ![example csv](images/example_csv.png "Example CSV Format")
- 
- ![example output](images/example_output.png "Example Output")
-
- ## To Do:
-   - [ ] Add support for different solvent and polymer systems
-   - [ ] Rework stats workup
+## To Do:
+  - [x] Rework stats for readability and ANOVA testing
--- soxhlet_workup.py
+++ soxhlet_workup.py
@@ -1,43 +1,63 @@
+#%%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.optimize import curve_fit
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
-# Load the data
-df = pd.read_csv(r'CSVs\240506_CB-PDMS-paper-revisions.csv',  header = 0)    
-# x_label = 'oven-laser-oven times (min)'
-
-# Define the constants
-phi_hexane = 0.39  
-V_hexane_molar = 130.7 # mL/mol 
-p_hexane = 0.661 # g/mL
-R = 8.3145 # J/mol K
+# Constants
+phi_hexane = 0.39  # dimensionless
+V_hexane_molar = 130.7  # mL/mol 
+p_hexane = 0.661  # g/mL
+R = 8.3145  # J/mol K
 T = 298.15  # K
 
+#%%
+# Load the data
+file_path = 'CSVs/240529_PDMS_paper-revisions.csv'
+df = pd.read_csv(file_path)
+
+# Calculations
 M_hexane = df['wash'] - df['pre-wash']
 V_hexane = M_hexane / p_hexane
 V_poly = df['pre-wash'] / (df['pre-wash'] + phi_hexane * V_hexane)
 
-n = - (np.log(1 - V_poly) + V_poly + phi_hexane * V_poly**2) / (V_hexane_molar * (V_poly**(1/3) - V_poly / 2))
-E = 3 * n * R * T # modulus ~ 7000 * n
+n = - (np.log(1 - V_poly) + V_poly + phi_hexane * V_poly**2) / (V_hexane_molar * (V_poly**(1/3) - V_poly / 2)) * 1000  # 1000 for mol --> mmol
+E = 3 * n * R * T  # modulus = ~7000 * n
 
 df['n'] = n
 df['E'] = E
 df['V_poly'] = V_poly
-df['group'] = df['sample']
 
-# # Statistical analysis (for repeated measures)
-# numerical_columns = df.select_dtypes(include=[np.number]).columns.tolist()
-# df = df.groupby('group')[numerical_columns].agg(['mean', 'std']).reset_index()
+# Correcting the sample type extraction to the specific sample types
+df['sample_type'] = df['sample'].str.extract(r'(ambient-pristine|ambient-1e-6|laser-1e-6)')[0]
 
+# Remove rows with NaN in 'sample_type' if any
+df = df.dropna(subset=['sample_type'])
+
+# Convert 'sample_type' to a categorical type with ordered categories
+df['sample_type'] = pd.Categorical(df['sample_type'], categories=df['sample_type'].unique(), ordered=True)
+
+# Grouping the data by the corrected sample type and calculating mean and std
+grouped_prefix_corrected = df.groupby('sample_type').agg({'n': ['mean', 'std']}).reset_index()
+grouped_prefix_corrected.columns = ['sample_type', 'n_mean', 'n_std']
+
+#%%
 # Plotting
 fontsize = 20
-fig, ax = plt.subplots(figsize=(12, 8))
-sns.barplot(x='sample', y='n', data=df, ax=ax, ci='sd')
-plt.xlabel('', fontsize=fontsize)
-plt.ylabel('n (mol/mL)', fontsize=fontsize)
+plt.figure(figsize=(12, 8))
+plt.bar(grouped_prefix_corrected['sample_type'], grouped_prefix_corrected['n_mean'], yerr=grouped_prefix_corrected['n_std'], capsize=5)
+plt.ylabel('Crosslink Density (mmol/cm^3)', fontsize=fontsize)
 plt.xticks(fontsize=fontsize)
 plt.yticks(fontsize=fontsize)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))  # Set y-axis to scientific notation
 plt.tight_layout()
+
+# Post-hoc test (Tukey's HSD)
+tukey = pairwise_tukeyhsd(endog=df['E'], groups=df['sample_type'], alpha=0.05)
+print("Tukey's HSD results:")
+print(tukey)
+
+# Plotting Tukey's HSD
+tukey.plot_simultaneous()
 plt.show()
+# %%
